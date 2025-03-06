7b958a4ce4932761cb175f91afc1c241df7c69d5
ritesh7092 <riteshrajtiwari.iiitbh@gmail.com>
2025-03-06 13:48:43 +0530

    Controller Layer Updated and User Defined Exceptions Added

---
+++ TurboHire/src/main/java/com/Rental/exception/CustomerLicenceException.java
@@ -0,0 +1,8 @@
+package com.Rental.exception;
+
+public class CustomerLicenceException extends RuntimeException {
+
+
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ TurboHire/src/main/java/com/Rental/exception/CustomerStatusException.java
@@ -0,0 +1,7 @@
+package com.Rental.exception;
+
+public class CustomerStatusException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+}
